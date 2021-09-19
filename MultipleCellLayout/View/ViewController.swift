2b8adc0da8cf9d92437dc223fd35b5851e4fd5f0
Jongho Lee <bazinga940@gmail.com>
2021-09-20 01:13:06 +0900

    builder 패턴 적용해보는중

--- MultipleCellLayout/View/ViewController.swift
+++ MultipleCellLayout/View/ViewController.swift
@@ -9,6 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	lazy var collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
+		.builder
+		.translatesAutoresizingMaskIntoConstraints(false)
+		.delegate(self)
+		.dataSource(self)
+		.backgroundColor(.clear)
+		.showsVerticalScrollIndicator(false)
+//		.apply {
+//			$0.register(sections: self.sectionControllers)
+//			self.viewModel.items.forEach {
+//				$0.registerCell(on: collectionView)
+//			}
+
+//			for configurator in self.viewModel.items {
+//				configurator.registerCell(on: $0)
+//			}
+//		}
+		.build()
+
+	lazy var collectionViewFlowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+		.builder
+		.scrollDirection(.vertical)
+		.build()
+
+	let viewModel = ViewModel()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
